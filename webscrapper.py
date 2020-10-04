8c3042ca84db28affa53342f226a185cd4df2fb1
Rishabh Bhardwaj <rishabhbhardwaj64@gmail.com>
2020-10-04 12:21:34 +0530

    Update webscrapper.py

--- webscrapper.py
+++ webscrapper.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 from selenium import webdriver
  
-chrome_path="A:/Programs/Python/Python 2.7/selenium/chromedriver"
+chrome_path="A:/Programs/Python/Python 3.7/selenium/chromedriver"
  
 driver=webdriver.Chrome(chrome_path)
  
